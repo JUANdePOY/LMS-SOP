a5403b40c4f159dea33f94d0831ba628bfc86111
juandepoy <aringjhonpaul@gmail.com>
2026-06-06 16:20:02 +0800

    fix the client side

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(cors({
 }));
 
 // Handle OPTIONS preflight explicitly
-app.options('*', cors());
+app.options('/:path(*)', cors());
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
