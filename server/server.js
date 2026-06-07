cb532afc6a408d357aa0a133ec71fd47c70db197
juandepoy <aringjhonpaul@gmail.com>
2026-06-07 20:08:04 +0800

    fix the error for express 5

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(cors({
 }));
 
 // Handle preflight requests
-app.options('*', cors());
+app.options('/*', cors());
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
