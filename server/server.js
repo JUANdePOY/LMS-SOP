259b9cb0275b6bb47150e71acf1f0f0deed7e27e
juandepoy <aringjhonpaul@gmail.com>
2026-06-07 20:12:46 +0800

    remove: app.options

--- server/server.js
+++ server/server.js
@@ -28,9 +28,6 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-// Handle preflight requests
-app.options('/*', cors());
-
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
